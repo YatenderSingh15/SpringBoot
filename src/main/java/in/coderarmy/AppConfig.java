710db0cdba9f3cd18f67222e9f0b9b54b5edc94f
Yatender <kumaraman76002@gmail.com>
2026-07-03 13:16:40 +0530

    commit

---
+++ src/main/java/in/coderarmy/AppConfig.java
@@ -0,0 +1,8 @@
+package in.coderarmy;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
--- src/main/java/in/coderarmy/OrderService.java
+++ src/main/java/in/coderarmy/OrderService.java
@@ -1,4 +1,20 @@
 package in.coderarmy;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderService {
+
+    private PaymentService paymentService = new PaymentService();
+
+    public OrderService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
+
+    public void placeOrder(){
+
+        payementService.pay();
+
+        System.out.println("Order placed");
+    }
 }
